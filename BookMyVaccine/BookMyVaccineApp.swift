2ba42d54e32aba1f53570a11a20c233e8aed03c3
NeilsUltimateLab <neilsultimatelab@icloud.com>
2021-02-20 00:51:48 +0530

    Initial Commit

---
+++ BookMyVaccine/BookMyVaccineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMyVaccineApp.swift
+//  BookMyVaccine
+//
+//  Created by Neil Jain on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BookMyVaccineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
