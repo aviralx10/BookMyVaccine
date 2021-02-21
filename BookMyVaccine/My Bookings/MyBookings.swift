4b98f18f9b7a1532cf0188b9bfd50265e185cec8
Kevin Peng <peng.kevin@me.com>
2021-02-21 05:45:41 -0500

    [FEATURE] My Bookings with Data

--- BookMyVaccine/My Bookings/MyBookings.swift
+++ BookMyVaccine/My Bookings/MyBookings.swift
@@ -5,66 +5,71 @@
 //  Created by Aviral Yadav on 20/02/21.
 //
 
+import Combine
 import SwiftUI
 import CoreImage.CIFilterBuiltins
 
 struct MyBookings: View {
-    //@Binding var isBooked : Bool
-    //@State var myString : String
+    @AppStorage("userID") var userID: String = ""
+    @StateObject private var viewModel = MyBookingsViewModel()
 
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     var body: some View {
-        ScrollView(.vertical){
-            VStack{
-                Text("MY BOOKINGS")
-                    .fontWeight(.heavy)
-                    .padding(20)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(viewModel.bookings) { booking in
                 VStack{
-                    Text("MY VENUE/HOSPITAL NAME")
+                    Text("MY BOOKINGS")
+                        .fontWeight(.heavy)
+                        .padding(20)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    VStack{
+                        Text("MY VENUE/HOSPITAL NAME")
+                            .fontWeight(.heavy)
+                            .frame(width: 210, height: 30, alignment: .center)
+                            .foregroundColor(.white)
+                            .padding(.all)
+                            .background(Color.blue)
+                            .cornerRadius(16)
+                        Text(booking.hospitalName)
+                            .fontWeight(.bold)
+                            .padding(10)
+                    }.padding(20)
+                    VStack{
+                        Text("MY TIME SLOT")
+                            .fontWeight(.heavy)
+                            .frame(width: 210, height: 30, alignment: .center)
+                            .foregroundColor(.white)
+                            .padding(.all)
+                            .background(Color.blue)
+                            .cornerRadius(16)
+                        Text(Date.timeFormatter.string(from: booking.time))
+                            .fontWeight(.bold)
+                            .padding(10)
+                        Text(Date.dateOnlyFormatter.string(from: booking.time))
+                            .fontWeight(.bold)
+                            .padding(10)
+                    }.padding(20)
+
+                    Text("CONTACTLESS QR CODE")
                         .fontWeight(.heavy)
                         .frame(width: 210, height: 30, alignment: .center)
-                        .foregroundColor(.white)
-                        .padding(.all)
-                        .background(Color.blue)
-                        .cornerRadius(16)
-                    Text("NEW GENERAL HOSPITAL")
-                        .fontWeight(.bold)
-                        .padding(10)
-                }.padding(20)
-                VStack{
-                    Text("MY TIME SLOT")
-                        .fontWeight(.heavy)
-                        .frame(width: 210, height: 30, alignment: .center)
-                        .foregroundColor(.white)
-                        .padding(.all)
-                        .background(Color.blue)
-                        .cornerRadius(16)
-                    Text("2-2:15 PM")
-                        .fontWeight(.bold)
-                        .padding(10)
-                }.padding(20)
+                    .foregroundColor(.white)
+                    .padding(.all)
+                    .background(Color.blue)
+                    .cornerRadius(16)
+                    Image(uiImage: generateQRCode(from: booking.qrCode))
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:200,height:200)
+                    Spacer()
 
-                Text("CONTACTLESS QR CODE")
-                    .fontWeight(.heavy)
-                    .frame(width: 210, height: 30, alignment: .center)
-                .foregroundColor(.white)
-                .padding(.all)
-                .background(Color.blue)
-                .cornerRadius(16)
-                Image(uiImage: generateQRCode(from: "Sample"))
-                    .interpolation(.none)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:200,height:200)
-                Spacer()
-
+                }
             }
         }
-
-
     }
+
     func generateQRCode(from string: String)->UIImage{
         let data = Data(string.utf8)
             filter.setValue(data, forKey: "inputMessage")
@@ -76,5 +81,56 @@ struct MyBookings: View {
             }
 
             return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+struct Booking: Identifiable {
+    let hospitalName: String
+    let qrCode: String
+    let time: Date
+
+    init(bookedAppointment: BookedAppointment, hospital: Hospital) {
+        self.hospitalName = hospital.name
+        self.qrCode = bookedAppointment.qrCode
+        self.time = bookedAppointment.time
+    }
+
+    var id: String {
+        qrCode
+    }
+}
+
+final class MyBookingsViewModel: ObservableObject {
+    var subscribers = Set<AnyCancellable>()
+    @Published var bookedAppointments = [BookedAppointment]()
+    @Published var hospitals: [Hospital] = []
+    var bookings: [Booking] {
+        bookedAppointments.compactMap { appointment in
+            let hospital = hospitals.first { $0.id == appointment.hospitalID }
+            if let hospital = hospital {
+                return Booking(bookedAppointment: appointment, hospital: hospital)
+            } else {
+                return nil
+            }
+        }
+    }
+
+    func findAppointments(forUser userID: String) {
+        let url = URL(string: "https://bookmyvaccine.herokuapp.com/patients/\(userID)/appointments")!
+        NetworkManager().fetch([BookedAppointment].self, from: url)
+            .replaceError(with: [])
+            .sink(receiveValue: { [self] in
+                self.bookedAppointments = $0
+                let arrayOfPublishers = $0.map { (appointment: BookedAppointment) -> AnyPublisher<Hospital, Error> in
+                    let url = URL(string: "https://bookmyvaccine.herokuapp.com/hospitals/\(appointment.hospitalID)")!
+                    return NetworkManager().fetch(Hospital.self, from: url)
+                }
+                Publishers.MergeMany(arrayOfPublishers)
+                    .collect()
+                    .replaceError(with: [])
+                    .assign(to: \.hospitals, on: self)
+                    .store(in: &subscribers)
+            })
+            .store(in: &subscribers)
     }
 }
